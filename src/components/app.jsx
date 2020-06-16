868e5a780897e387265a5d6ad6e87588eac3e0ff
Vladimir Kharlampidi <nolimits4web@gmail.com>
2020-06-16 14:30:21 +0300

    Create F7 project

---
+++ src/components/app.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import {
+  App,
+  View,
+} from 'framework7-react';
+
+import routes from '../js/routes';
+
+export default class extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      // Framework7 Parameters
+      f7params: {
+        name: 'My App', // App name
+        theme: 'auto', // Automatic theme detection
+
+
+
+        // App routes
+        routes: routes,
+      },
+
+    }
+  }
+  render() {
+    return (
+      <App params={ this.state.f7params } >
+
+        {/* Your main view, should have "view-main" class */}
+        <View main className="safe-areas" url="/" />
+
+      </App>
+    );
+  }
+
+  componentDidMount() {
+    this.$f7ready((f7) => {
+
+      // Call F7 APIs here
+    });
+  }
+}
